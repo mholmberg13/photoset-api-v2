ff0c4306a20356e09be35bb82cc9140ae8895fbb
Matt Holmberg <mattholmberg@Matts-MacBook-Pro.local>
2020-05-27 12:05:25 -0500

    heroku ts

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,5 @@
 Cloudinary.config do |config|    
-    config.cloud_name = photosetcloud   
+    config.cloud_name = "photosetcloud"   
     config.api_key = "117987468637986"   
     config.api_secret = "CSI38LG336dey0U-TxvEkyySicc"   
     config.secure = true    
